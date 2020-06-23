c4165d81941098e3574abfe71372d47d5ea48647
Rodrigo Gregorio <rgregorio_2014@hotmail.com>
2020-06-22 21:55:35 -0300

    Projects Crud

--- src/app.js
+++ src/app.js
@@ -1,16 +1,35 @@
 const express = require("express");
 const cors = require("cors");
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use('/repositories/:id', validateRepositoryId);
+app.use('/repositories', countRequest);
 
 const repositories = [];
 
+function countRequest(request, response, next) {
+    const { method, url } = request;
+    const log = `[${method.toUpperCase()}] ${url}`;
+
+    console.log(log);
+    next();
+}
+
+function validateRepositoryId(request, response, next) {
+    const { id } = request.params;
+
+    if (!isUuid(id)) {
+        return response.status(400).json({ error: "Invalid id" });
+    }
+    return next();
+}
+
 app.get("/repositories", (request, response) => {
-    // TODO
+    return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
@@ -31,11 +50,35 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-    // TODO
+    const { id } = request.params;
+    const { title, url, techs } = request.body;
+
+    const repository = repositories.find(repository => repository.id == id);
+
+    if (!repository) {
+        return response.status(400).send();
+    }
+
+    repository.title = title;
+    repository.url = url;
+    repository.techs = techs;
+
+    return response.json(repository);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-    // TODO
+    const { id } = request.params;
+
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+    if (repositoryIndex < 0) {
+        return response.status(400).json({ error: "Repository not found" });
+    }
+
+    repositories.slice(repositoryIndex, 1);
+
+    return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
